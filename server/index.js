5a178f92838ec2e5bdaca27b092574b0ceb1f4ab
Benjamin Stephens <17017460+speckospock@users.noreply.github.com>
2017-10-11 15:32:36 -0700

    Skeletons postgres db model

--- server/index.js
+++ server/index.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../react-client/dist'));
 
-//This function is to retrieve the top 5 recommended shows from the MovieDB api for the front page of WatchBuddy.
 app.get('/recommend', routes.recommend);
 
 app.post('/:route', (req, res) => {
@@ -19,20 +18,6 @@ app.post('/:route', (req, res) => {
 	routes[req.params.route](req, res);
 });
 
-// app.post('/update', function (req, res) {
-
-// })
-
 app.listen(process.env.PORT || 3000, ()  => {
   console.log(`listening on port ${3000}!`);
-})
-
-
-
-
-
-
-
-
-
-
+});
